3f6409662c14f474028d5d439a8dc30d861580cf
Maurilio Menezes <mauriliosm25@gmail.com>
2022-09-04 17:46:36 -0300

    feat: Novos atributos e getters e setters

--- java/MM'S-Java/src/Cliente.java
+++ java/MM'S-Java/src/Cliente.java
@@ -1,16 +1,19 @@
 public class Cliente {
 
   private int id;
+  private String CPF;
   private String nome;
   private String Email;
 
   public Cliente() {
   }
 
-  public Cliente(int id, String nome, String Email) {
+  public Cliente(int id, String nome, String CPF, String Email) {
     this.id = id;
+    this.CPF = CPF;
     this.nome = nome;
     this.Email = Email;
+
   }
 
   public int getId() {
@@ -35,6 +38,14 @@ public class Cliente {
 
   public void setEmail(String Email) {
     this.Email = Email;
+  }
+
+  public String getCPF() {
+    return this.CPF;
+  }
+
+  public void setCPF(String CPF) {
+    this.CPF = CPF;
   }
 
 }

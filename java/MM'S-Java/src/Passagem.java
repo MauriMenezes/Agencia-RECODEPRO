9fb7fa3e5f15564e9e76b1a5984a66951f1722f7
Maurilio Menezes <mauriliosm25@gmail.com>
2022-09-04 17:48:38 -0300

    feat: Novos atributos e getters/setters

--- java/MM'S-Java/src/Passagem.java
+++ java/MM'S-Java/src/Passagem.java
@@ -1,21 +1,22 @@
 public class Passagem {
 
-  private int id;
   private Cliente user;
   private String partida;
   private String destino;
+  private String data_hora;
   private CompanhiaAerea companhia;
-  private String valor;
 
   public Passagem() {
   }
 
-  public void compraPassagem(Cliente cliente, String partida, String destino, CompanhiaAerea companhiaAerea) {
+  public void compraPassagem(Cliente cliente, String partida, String destino, String data_hora,
+      CompanhiaAerea companhiaAerea) {
 
     this.user = cliente;
     this.destino = destino;
     this.partida = partida;
     this.companhia = companhiaAerea;
+    this.data_hora = data_hora;
   }
 
   public String consulta() {
@@ -23,4 +24,45 @@ public class Passagem {
     return "USUARIO: " + this.user.getNome() + " || " + " Origem:  " + this.partida + " || " + " DESTINO: "
         + this.destino + " || " + "Companhia: " + this.companhia.getNome();
   }
+
+  public Cliente getUser() {
+    return this.user;
+  }
+
+  public void setUser(Cliente user) {
+    this.user = user;
+  }
+
+  public String getPartida() {
+    return this.partida;
+  }
+
+  public void setPartida(String partida) {
+    this.partida = partida;
+  }
+
+  public String getDestino() {
+    return this.destino;
+  }
+
+  public void setDestino(String destino) {
+    this.destino = destino;
+  }
+
+  public String getData_hora() {
+    return this.data_hora;
+  }
+
+  public void setData_hora(String data_hora) {
+    this.data_hora = data_hora;
+  }
+
+  public CompanhiaAerea getCompanhia() {
+    return this.companhia;
+  }
+
+  public void setCompanhia(CompanhiaAerea companhia) {
+    this.companhia = companhia;
+  }
+
 }

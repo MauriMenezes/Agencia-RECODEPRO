449616f78d9f3f9145da3f5cd9a29e6d473ed1c5
Maurilio Menezes <mauriliosm25@gmail.com>
2022-09-05 16:57:05 -0300

    Teste das classes no console

--- java/MM'S-Java/src/App.java
+++ java/MM'S-Java/src/App.java
@@ -1,16 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
-        Cliente cliente = new Cliente();
-        cliente.setEmail("MAURILIOSM25@gmail.com");
-        cliente.setNome("MAURILIO");
-        cliente.setId(1);
-        CompanhiaAerea companhiaAerea = new CompanhiaAerea();
-        companhiaAerea.setNome("GOL");
-        Passagem compraPassagem = new Passagem();
-        compraPassagem.compraPassagem(cliente, "SALVADOR", "SAO-PAULO", companhiaAerea);
+        Cliente cliente = new Cliente(1, "MAURILIO", "888.888.888-00", "MAURILIOSM25@gmail.com");
+        Companhia companhia = new Companhia(1, "GOL");
 
-        System.out.println(compraPassagem.consulta());
+        Passagem passagem = new Passagem();
+        passagem.compraPassagem(cliente, "SALVADOR", "SAO PAULO", "04/09/2022", companhia);
+        System.out.println(passagem.consulta());
+
+        // // ADD Cliente no BD
+        // ClienteDAO clienteDAO = new ClienteDAO();
+        // clienteDAO.saveCliente(cliente);
+
+        // // remove
+        // clienteDAO.removeByid(1);
+
+        // // UPDATE
+        // clienteDAO.update(cliente);
+        // // LIST
+        // for (cliente cliente : clienteDAO.getClientes()) {
+        // System.out.println(cliente);
+        // }
     }
 }
